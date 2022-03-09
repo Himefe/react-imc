7bec7764769015b931453fe4b7056f7a1610e0d0
Himefe <idanger2015@hotmail.com>
2022-03-09 20:21:15 -0300

    Ajustando valores do imc

--- src/imc.tsx
+++ src/imc.tsx
@@ -9,15 +9,17 @@ type Level = {
 };
 
 const levels: Level[] = [
-  { title: "Magreza", color: "#96A3AB", icon: "down", imc: [0, 18.5] },
-  { title: "Normal", color: "#0EAD69", icon: "up", imc: [18.6, 24.9] },
-  { title: "Sobrepeso", color: "#E2B039", icon: "down", imc: [25.0, 30] },
+  { title: "Magreza", color: "#96A3AB", icon: "down", imc: [0, 18.59] },
+  { title: "Normal", color: "#0EAD69", icon: "up", imc: [18.6, 24.99] },
+  { title: "Sobrepeso", color: "#E2B039", icon: "down", imc: [25.0, 30.09] },
   { title: "Obesidade", color: "#C3423E", icon: "down", imc: [30.1, 99] },
 ];
 
 const calculoImc = (height: number, weight: number) => {
   if (height !== undefined && weight !== undefined) {
     let imc = weight / (height * height);
+
+    console.log(imc);
 
     for (let index in levels) {
       if (imc >= levels[index].imc[0] && imc <= levels[index].imc[1]) {
